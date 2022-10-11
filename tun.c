c2b2da1003157fb18fc2411971dd3d20e90766ad
GuoHaoLan <coopticks@hotmail.com>
2022-10-11 17:18:53 +0800

    Test

--- tun.c
+++ tun.c
@@ -99,9 +99,9 @@ static int setup_socket(struct ring *ring, char *netdev)
 static void display(struct tpacket3_hdr *ppd)
 {
 	//struct ethhdr *eth = (struct ethhdr *) ((uint8_t *) ppd + ppd->tp_mac);
-	struct iphdr *ip = (struct iphdr *) ((uint8_t *) ppd);
+	struct iphdr *ip = (struct iphdr *) ((uint8_t *) ppd + ppd->tp_mac);
 
-	printf("%d\n", ip);
+	printf("%d\n", ip->total_length);
 
 
 	//printf("rxhash: 0x%x\n", ppd->hv1.tp_rxhash);
