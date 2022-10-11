444a19ce6e108f220ea44e15a5c076a664f31ba7
GuoHaoLan <coopticks@hotmail.com>
2022-10-11 22:57:30 +0800

    Test

--- tun.c
+++ tun.c
@@ -117,7 +117,7 @@ static void copy_to_buf(struct Context *ctx, struct tpacket3_hdr *ppd)
 	unsigned int randint;
 	uint16_t      binlen;
 	rand_sse(&len, 16);
-	len = MAX(MIN((unsigned)1500 - ppd->tp_len, len), (unsigned) 16);
+	len = MAX(MIN((unsigned)(1500 - ppd->tp_len), len), (unsigned) 16);
 	/*
 	for (int i=0, j=0; i < len; i += 16, j += 2) {
 		srand_sse((unsigned) time(NULL) + i);
