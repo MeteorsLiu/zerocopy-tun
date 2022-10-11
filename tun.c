10ad2f221196da1eeaf417d8cf48b5ee63442971
GuoHaoLan <coopticks@hotmail.com>
2022-10-11 16:55:38 +0800

    Test

--- tun.c
+++ tun.c
@@ -101,7 +101,7 @@ static void display(struct tpacket3_hdr *ppd)
 	//struct ethhdr *eth = (struct ethhdr *) ((uint8_t *) ppd + ppd->tp_mac);
 	struct iphdr *ip = (struct iphdr *) ((uint8_t *) ppd + ppd->tp_mac);
 
-	printf("%d\n", eth->h_proto);
+	printf("%d\n", ip->saddr);
 
 
 	//printf("rxhash: 0x%x\n", ppd->hv1.tp_rxhash);
