afa8c5ff77839d20be7009ee14a37ffd49388527
GuoHaoLan <coopticks@hotmail.com>
2022-10-11 15:29:01 +0800

    T

---
+++ tun.h
@@ -0,0 +1,32 @@
+#ifndef tun_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <signal.h>
+#include <inttypes.h>
+#include <sys/socket.h>
+#include <sys/mman.h>
+#include <linux/if_packet.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/if_tun.h>
+#include <sys/epoll.h>
+
+#define _GNU_SOURCE 
+#include <fcntl.h>
+
+#ifndef likely
+# define likely(x)		__builtin_expect(!!(x), 1)
+#endif
+#ifndef unlikely
+# define unlikely(x)		__builtin_expect(!!(x), 0)
+#endif
+
+
+#endif
